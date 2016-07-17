740d3a8e9c280a853a728a2cf29cc4dc64525f27
Joseph Long <joseph@looker.com>
2016-07-16 18:18:39 -0700

    Add files via upload

---
+++ storingstrings.js
@@ -0,0 +1,11 @@
+var introOne;
+var introTwo;
+
+introOne = 'Hello World';
+introTwo = 'Greetings';
+
+var elTamale = document.getElementById('tamale');
+elTamale.textContent = introOne;
+
+var elFaja = document.getElementById('faja');
+elFaja.textContent = introTwo;
